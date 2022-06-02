8278d90331c0197ebaf9c997c053f5ddd5ec660b
Ryan Fleming <ryanfleming93@aol.com>
2022-06-02 15:00:49 +0100

    homepage, mobile and landing page updates

---
+++ app.js
@@ -0,0 +1,8 @@
+function activeMenu() {
+    const element = document.getElementById("menu-options");
+    if (element.className == "off") {
+      element.className = "newStyle";
+    } else {
+      element.className = "myStyle";
+    }
+  }
