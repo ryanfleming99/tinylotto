647947b1d1ecf1096ae01d3f79f2bc379708b620
Ryan Fleming <ryanfleming93@aol.com>
2022-09-06 22:25:37 +0100

    added google tag manager

--- app.js
+++
@@ -1,8 +0,0 @@
-function activeMenu() {
-  const element = document.getElementById("menu-options");
-  if (element.className == "off") {
-    element.className = "newStyle";
-  } else {
-    element.className = "myStyle";
-  }
-}
